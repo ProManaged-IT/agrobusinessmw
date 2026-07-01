604a34a0e00f33b81ec2a09889bb1fc639875c84
papauri <johnpaulchirwa@gmail.com>
2026-07-01 10:41:30 +1000

    changes

--- unregister.js
+++
@@ -1,9 +0,0 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    for (let registration of registrations) {
-      registration.unregister().then(() => {
-        console.log('Service Worker unregistered');
-      });
-    }
-  });
-}
